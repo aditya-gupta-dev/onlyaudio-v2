608911145a2189878653753a8ea80e2230403571
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-08-16 10:43:24 +0530

    Update App.jsx
    Added Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
+import Settings from './components/Settings';
 
 export default function App() {
     
   return (
-    <Home></Home>    
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <Home/> }/>
+        <Route path='/settings' element={ <Settings/> }/>
+      </Routes>
+    </BrowserRouter>
   );
 }
