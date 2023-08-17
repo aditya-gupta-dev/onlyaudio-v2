c2287f0d5d470b9f8c54d8ff3af96dc4048cacf0
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-08-17 23:02:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
+import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Settings from './components/Settings';
 
 export default function App() {
-    
+  
+  const [theme, setTheme] = useState("light");
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={ <Home/> }/>
-        <Route path='/settings' element={ <Settings/> }/>
+        <Route path='/' element={ <Home currentTheme={theme} setCurrentTheme={setTheme}/> }/>
+        <Route path='/settings' element={ <Settings currentTheme={theme} setCurrentTheme={setTheme}/> }/>
       </Routes>
     </BrowserRouter>
   );
