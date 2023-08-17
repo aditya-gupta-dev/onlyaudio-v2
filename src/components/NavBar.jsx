d57eb122df5d3cb0d277ab08745e747b7267b8cb
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-08-17 20:49:44 +0530

    Added Navigation Link component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,18 @@
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
-export default function NavBar({currentTheme, setCurrentTheme}) {
-
-    const toggleTheme = () => {
-        if(currentTheme === 'light') {
-            setCurrentTheme('dark');
-            localStorage.setItem('theme', 'dark');
-        } else {
-            setCurrentTheme('light');
-            localStorage.setItem('theme', 'light');
-        }
-    }
+export default function NavBar() {
 
     return (
         <div className='sticky top-0 w-full'>
             <div className='navbar sticky bg-base-300'>
                 <a className='btn btn-ghost normal-case text-2xl font-bold'>Only Audio</a>
                 <div className='navbar-end'>
-                    <button className='btn btn-circle shadow-2xl' onClick={toggleTheme}>
+                    <Link to="/settings" className="btn btn-circle shadow-2xl">
                         <svg xmlns='http://www.w3.org/2000/svg' fill='#ffffff' width='32' height='32' viewBox='0 0 24 24' stroke='currentColor'>
                             <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='3' d='M4 6h16M4 12h16M4 18h7'/>
                         </svg>
-                    </button>
+                    </Link>
                 </div>
             </div> 
         </div>
